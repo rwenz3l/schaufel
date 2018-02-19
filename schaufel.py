478d2f8fc3bed64f6b625e77e87dd0944ad9116f
Robin Wenzel <info@robin-wenzel.de>
2018-02-19 03:18:07 +0100

    Init
    Signed-off-by: Robin Wenzel <info@robin-wenzel.de>

---
+++ schaufel.py
@@ -0,0 +1,83 @@
+#! /usr/bin/python3
+
+import os
+import signal
+import subprocess
+import requests
+import atexit
+import json
+from requests.exceptions import ConnectionError
+from flask import Flask, render_template, jsonify, request
+from pprint import pprint as pp
+from elasticsearch import Elasticsearch
+
+# CONSTANTS
+LIBRARY_PATH = "/Users/rwenzel/Documents/eBooks"
+ELASTIC_INDEX = 'schaufel'
+
+# FLASK
+app = Flask(__name__)
+app.secret_key = 'SchaufelKey'
+
+# INIT
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+
+@app.route('/')
+def root():
+    """
+    WebApp Root
+    :return: index page with random result
+    """
+    random = {"size": 1, "query": {"function_score": {"functions": [{"random_score": {"seed": "1477072619038"}}]}}}
+    res = es.search(index=ELASTIC_INDEX, doc_type="book", body=random)
+    return render_template('index.html', results=res['hits']['hits'])
+
+
+@app.route('/es_status')
+def test_elasticsearch():
+    try:
+        res = requests.get('http://localhost:9200')
+        j = json.loads(res.content)
+        return jsonify(j)
+    except ConnectionError:
+        print('Connection Error')
+    return 'ElasticSearch is offline'
+
+
+@app.route('/search', methods=['POST'])
+def search_books():
+    q = request.form['query']
+    query = {"query": {"match": {"filename": {"query": q, "operator": "and"}}}}
+    res = es.search(index=ELASTIC_INDEX, doc_type="book", body=query)
+    return render_template('search.html', results=res['hits']['hits'])
+
+
+@app.route('/index')
+def index_books():
+    # Delete old Index
+    es.indices.delete(index=ELASTIC_INDEX, ignore=[400, 404])
+    # Read ES-Config from File
+    with open('es_schaufel_index.json') as json_data:
+        d = json.load(json_data)
+        json_data.close()
+    # Create new Index
+    es.indices.create(index=ELASTIC_INDEX, body=d, ignore=400)
+    for r, dirs, files in os.walk(LIBRARY_PATH):
+        for file in files:
+            file_name, file_ext = os.path.splitext(file)
+            file_path = os.path.join(r, file)
+            if file_ext in ('.pdf', '.epub', '.mobi'):
+                print("Add to Index: %s" % file)
+                es.index(index=ELASTIC_INDEX, doc_type="book", body={"filename": file_name, "filetype": file_ext,
+                                                                     "filepath": file_path})
+    return 'Library Index updated'
+
+
+if __name__ == '__main__':
+    app.debug = True
+    if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        # Prepare Stuff here
+        pass
+    # Start Flask App
+    app.run(host="0.0.0.0", port=5000)
---
+++ static/js/user.js
@@ -0,0 +1,35 @@
+$(function() {
+  var timer;
+  $("#search_input").keyup(function() {
+    clearTimeout(timer);
+    var ms = 10; // milliseconds
+    var val = this.value;
+    timer = setTimeout(function() {
+      search();
+    }, ms);
+  });
+});
+
+function search(){
+    var query = document.getElementById("search_input").value;
+    console.log(query)
+    $.ajax({
+        type: 'post',
+        data: { 'query': query},
+        url: '/search',
+        success: function (response) {
+            // We get the element having id of display_info and put the response inside it
+            $( '#search_result' ).html(response);
+        }
+    });
+}
+
+function reIndex(){
+$.ajax({
+        type: 'get',
+        url: '/index',
+        success: function (response) {
+            console.log("Re-Indexed")
+        }
+    });
+}
